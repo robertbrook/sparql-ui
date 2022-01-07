1aaa6dcb677b532a5b80294f8aba5e2c693284e9
Robert Brook <robertbrook@fastmail.fm>
2022-01-07 17:36:17 +0000

    Update converter.php

--- converter.php
+++ converter.php
@@ -48,7 +48,7 @@
     if (!isset($_REQUEST['raw'])) {
         print "<html>\n";
         print "<head><title>EasyRdf Converter</title><link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/water.css@2/out/water.css\">\n";
-        print '<script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.0/codemirror.min.js" integrity="sha512-JpMCZgesTWh1iu/8ujURbwkJBgbgFWe3sTNCHdIuEvPwZuuN0nTUr2yawXahpgdEK7FOZUlW254Rp7AyDYJdjg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>\n';
+        print '<script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.0/codemirror.min.js" integrity="sha512-JpMCZgesTWh1iu/8ujURbwkJBgbgFWe3sTNCHdIuEvPwZuuN0nTUr2yawXahpgdEK7FOZUlW254Rp7AyDYJdjg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>';
         print '<script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.0/codemirror.js" integrity="sha512-dK6guy/5KfuGFyZqGjtWr1HH8AGkI9UGZKD0uB9EDivJHt3dLSDgTteU0lsY4HtYbi3YhYnoKWQ5EfPS9TRCDg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>';
         print "</head>\n";
         print "<body>\n";
